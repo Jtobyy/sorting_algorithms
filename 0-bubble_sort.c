8b8b2f1e8d44f4ade459cb42229422614a857e25
Jtobyy <jtobi8161@gmail.com>
2021-10-27 02:58:49 +0000

    Implementation of bubble sort in progress

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,15 +12,15 @@ void bubble_sort(int *array, size_t size)
 {
 size_t i;
 int tmp;
-for (i = 0; i < size; i++)
+for (i = 0; i < size - 1; i++)
 {
 if (array[i] > array[i+1])
 {
 tmp = array[i];
 array[i] = array[i+1];
 array[i+1] = tmp;
-}
 print_array(array, size);
+}
 }
 return;
 }
