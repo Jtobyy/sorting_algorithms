a64ba761aa7599b76bb8549240af06d62c443fa9
Jtobyy <jtobi8161@gmail.com>
2021-10-27 03:11:55 +0000

    Implement bubble sort in 0-bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,5 +22,25 @@ array[i+1] = tmp;
 print_array(array, size);
 }
 }
+if (is_sorted(array, size) != 0)
+bubble_sort(array, size);
 return;
+}
+
+/**
+ *is_sorted: checks if an array of integeres is sorted
+ *
+ *@array: the array of integers
+ *@size: size of array
+ *return: zero if sorted or 1 if not sorted
+ */
+int is_sorted(int *array, size_t size)
+{
+size_t i;
+for (i = 0; i < size - 1; i++)
+{
+if (array[i] > array[i+1])
+return 1;
+}
+return 0;
 }
--- sort.h
+++ sort.h
@@ -21,5 +21,6 @@ struct listint_s *next;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+int is_sorted(int *array, size_t size);
 
 #endif
